90bc580df16cff02e6d0e9943321cb092f69a401
Gabriel Ferreira <57634913+gabrielvferreira@users.noreply.github.com>
2022-05-16 08:00:15 -0300

    Delete src directory
    Diretório deletado para atualização

--- src/main/java/br/com/condominio/condominio/repositories/MoradorRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.condominio.condominio.repositories;
-
-import br.com.condominio.condominio.entity.Morador;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MoradorRepository extends JpaRepository<Morador, Long> {
-}
